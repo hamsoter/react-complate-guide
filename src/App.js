9ce72b2ba572003e24a8db88818d2da81dff7d8a
hamsoter <omilzmil0w0@gmail.com>
2022-03-11 23:47:37 +0900

    props를 활용해 컴포넌트간 데이터 전달 및 출력

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "푸키몬빵",
+      amount: 1500,
+      date: new Date(2022, 2, 11),
+    },
+    {
+      id: "e2",
+      title: "안쥬쨩피규어",
+      amount: 250000,
+      date: new Date(2022, 2, 11),
+    },
+    {
+      id: "e3",
+      title: "데레 10연",
+      amount: 50000,
+      date: new Date(2022, 2, 11),
+    },
+    {
+      id: "e4",
+      title: "커미션",
+      amount: 50000,
+      date: new Date(2022, 2, 11),
+    },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
-      <ExpenseItem></ExpenseItem>
-      <ExpenseItem></ExpenseItem>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
