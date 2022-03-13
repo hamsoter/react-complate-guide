039e67b90298668662c046ec2fc297ed775995d9
hamsoter <omilzmil0w0@gmail.com>
2022-03-13 20:36:58 +0900

    Feat: 폼 인풋에 onChange핸들러 추가 + state 저장
    - form태그 input에 state를 업데이트하는 onChange함수를 추가

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,21 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExepnseForm = () => {
+  const [enterTitle, setEnterTitle] = useState("");
+  const [enterAmount, setEnterAmount] = useState("");
+  const [enterDate, setEnterDate] = useState("");
+
+  const titleChangeHandler = (e) => {
+    setEnterTitle(e.target.value);
+    console.log(enterTitle);
+  };
+
+  const AmountChangeHandler = (e) => {
+    setEnterAmount(e.target.value);
+    console.log(enterAmount);
+  };
+
+  const DateChangeHandler = (e) => {
+    setEnterDate(e.target.value);
+    console.log(enterDate);
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>지출명</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>가격</label>
-          <input type="number" min="0" step="1" />
+          <input
+            type="number"
+            min="0"
+            step="1"
+            onChange={AmountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>지출일</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={DateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense_actions">
